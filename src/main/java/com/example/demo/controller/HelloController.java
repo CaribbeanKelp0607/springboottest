b22699c92582d29a421c3b151c50366c2daf9249
alvin <302767720@qq.com>
2018-07-07 22:32:48 +0800

    test

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     private String index(){
+        //hello world
         return "Hello World!";
     }
 }
